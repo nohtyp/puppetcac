c616d7b02815563866eb8a7f82edda30de5e60e8
Thomas Foster <thomas.foster@noaa.gov>
2015-06-03 12:57:37 -0400

    modified yaml and sambacron for more frequent time, and using hiera for password

--- puppet/modules/samba/manifests/sambacron.pp
+++ puppet/modules/samba/manifests/sambacron.pp
@@ -4,6 +4,6 @@ class samba::sambacron inherits samba {
     command => "/usr/bin/wbinfo -u > /tmp/adjoined 2> /dev/null",
     require => [ File['adjoined file'], Exec['authconfig']],
     user    => root,
-    minute  => '*/5',
+    minute  => '*/2',
   }
 }
