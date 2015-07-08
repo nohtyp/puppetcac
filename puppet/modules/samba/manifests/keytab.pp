248e060ca7c51ce9ab1de9cf409bf9a625439143
nohtyp <thomas.foster80@gmail.com>
2015-07-08 11:59:31 -0400

    Update keytab.pp
    Added /bin path for rhel 6

--- puppet/modules/samba/manifests/keytab.pp
+++ puppet/modules/samba/manifests/keytab.pp
@@ -1,6 +1,6 @@
 class samba::keytab inherits samba {
   exec { 'Create keytab file':
-    path    => '/usr/bin',
+    path    => '/bin:/usr/bin',
     command => "net ads keytab create -U $join_user%$join_user_pw",
     unless  => "grep $join_user /tmp/adjoined",
   }
